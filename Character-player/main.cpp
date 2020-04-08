251457644517b769b8be5e3acb4d9f3d934f20da
gongluck <1039994845@qq.com>
2020-04-08 11:41:09 +0800

    任务管理器播放初版

--- Character-player/main.cpp
+++ Character-player/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-#include <thread>
-#include <atomic>
 
 #ifdef _WIN32
 #include <Windows.h>
+#include <tchar.h>
 #define ssize_t SSIZE_T
 #endif
 #include <vlc/vlc.h>
+
+#include "../../Code-snippet/cpp/process/process.h"
 
 #define CHECKEQUALRET(ret, compare)\
 if(ret == compare)\
@@ -33,9 +34,27 @@ int main(int argc, char** argv)
     libvlc_media_player_t* player_ = nullptr;
     libvlc_media_list_t* list_ = nullptr;
     libvlc_media_list_player_t* plist_ = nullptr;
-    HWND wnd_ = reinterpret_cast<HWND>(GetDesktopWindow());
-    int ret = 0;
+    HWND wnd_ = nullptr;
+    TCHAR classname[MAX_PATH] = { 0 };
 
+    std::vector<gprocess::ProcessInfo> result;
+    gprocess::WindowInfo windowinfo;
+    auto ret = gprocess::gethandle("Taskmgr.exe", result);
+    CHECKNEQUALRET(ret, 0);
+    windowinfo.processid = result[0].processid;
+    ret = gprocess::getallwindows(&windowinfo);
+    CHECKNEQUALRET(ret, 0);
+
+    for (int i = 0; i < windowinfo.childs.size(); ++i)
+    {
+        GetClassName(reinterpret_cast<HWND>(windowinfo.childs[i]->window), classname, _countof(classname));
+        if (_tcscmp(classname, TEXT("TaskManagerWindow")) == 0)
+        {
+            std::cout << windowinfo.childs[i] << std::endl;
+            wnd_ = reinterpret_cast<HWND>(windowinfo.childs[i]->window);
+        }
+    }
+    
     inst_ = libvlc_new(0, nullptr);
     CHECKEQUALRET(inst_, nullptr);
     media_ = libvlc_media_new_path(inst_, argc <= 1 ? "badapple.mp4" : argv[1]);
